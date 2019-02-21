107275cbb4c29f4477ae4873d0682fcbb9f3c327
Chaitanya <44492694+chaitanyarajugithub@users.noreply.github.com>
2019-02-21 18:27:07 +0530

    Update OwnerControllerTests.java

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTests.java
@@ -100,7 +100,7 @@ public class OwnerControllerTests {
     }
     
 
-  /*  @Test
+    @Test
     public void testProcessFindFormSuccess() throws Exception {
         given(this.clinicService.findOwnerByLastName("")).willReturn(Lists.newArrayList(george, new Owner()));
 
@@ -108,7 +108,7 @@ public class OwnerControllerTests {
             .andExpect(status().isOk())
             .andExpect(view().name("owners/ownersList"));
     }
-*/    
+    
 
     @Test
     public void testProcessFindFormByLastName() throws Exception {
